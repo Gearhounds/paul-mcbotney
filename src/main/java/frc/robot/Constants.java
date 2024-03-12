0e29b523c0308447f9a9c8541283d819cce8a285
babbittw-techsmith <w.babbitt@techsmith.com>
2024-03-12 00:30:36 -0400

    Added constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+public final class Constants {
+
+    // constructor
+    private Constants() {
+        // prevents instantiation as an object
+    }
+
+    // constants are static so they can be referenced off the class, final so they are constant, and public so we can
+    // access them outside of the class. Convention is to use uppercase snake_case when naming constants. We want to
+    // use constants in place of all magic numbers (apart from 0), for example in things like motor speeds or, soft limits, etc.
+    // By using this file, we can change them all at once if we need to tune the robot in anyway.
+
+    public static final int TEAM_NUMBER = 5674;
+
+    // add more constants as needed
+}
